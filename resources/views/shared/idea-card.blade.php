322015d7045fce788cc423af4b1e7f0e442fa95c
Jerome Nel <100586550+WebDevJerome@users.noreply.github.com>
2023-11-19 21:09:25 +0800

    Add update/edit functionality (CRUD)

--- resources/views/shared/idea-card.blade.php
+++ resources/views/shared/idea-card.blade.php
@@ -13,16 +13,34 @@
                 <form action="{{ route('ideas.destroy', $idea->id) }}" method="post">
                     @csrf
                     @method('delete')
-                    <a href="{{ route('ideas.show', $idea->id) }}" class="btn btn-info btn-sm">?</a>
+                    <a href="{{ route('ideas.show', $idea->id) }}" class="btn btn-info btn-sm">View</a>
+                    <a href="{{ route('ideas.edit', $idea->id) }}" class="btn btn-warning btn-sm">Edit</a>
                     <button class="btn btn-danger btn-sm">X</button>
                 </form>
             </div>
         </div>
     </div>
     <div class="card-body">
-        <p class="fs-6 fw-light text-muted">
-            {{ $idea->content }}
-        </p>
+        @if ($editing ?? false)
+            <form action="{{ route('ideas.update', $idea->id) }}" method="post">
+                @csrf
+                @method('put')
+                <div class="mb-3">
+                    <textarea name="content" class="form-control" id="content" rows="3">{{ $idea->content }}</textarea>
+                    @error('content')
+                    <span class="fs-6 text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="">
+                    <button type="submit" class="btn btn-warning mb-3"> Update </button>
+                </div>
+            </form>
+        @else
+            <p class="fs-6 fw-light text-muted">
+                {{ $idea->content }}
+            </p>
+        @endif
+
         <div class="d-flex justify-content-between">
             <div>
                 <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-heart me-1">
